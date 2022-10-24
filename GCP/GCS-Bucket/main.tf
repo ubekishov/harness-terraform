ae68507418d5b2bec59199a0e99c92ddc519091e
ubekishov <ubekishov@users.noreply.github.com>
2022-10-24 13:40:45 -0400

    no need for variables tf file

--- GCP/GCS-Bucket/main.tf
+++ GCP/GCS-Bucket/main.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region = "us-east-2"
-  }
-resource "aws_s3_bucket" "b" {
-  bucket = "${var.s3_bucket_name}"
-  tags = {
-    Name = "sandp-global"
-    Environment = "Dev"
-    }
-  }
 
-output "s3_bucket" {
-  value = aws_s3_bucket.b.bucket
-  }
+# Create new storage bucket in the US multi-region
+# with coldline storage
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "static" {
+  name          = "${random_id.bucket_prefix.hex}-new-bucket"
+  location      = "US"
+  storage_class = "COLDLINE"
+
+  uniform_bucket_level_access = true
+}
